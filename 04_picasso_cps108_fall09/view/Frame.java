6de4741ad623952dcf72d2e1de0a82d2bda4ac3c
shedlickj <shedlickj@f0a4c0ce-a3c5-11de-bea2-1db044c32224>
2009-09-25 05:39:50 +0000

    Updates: -Handling of the expression is now done in an "InputHandler" class, not in TextPanel. TextPanel calls an object of this class to do the evaluations and update the pixmap. -Frame has a menubar on top, buttons are being added (Open does not work yet). -Frame has a new preview button, which will open a new smaller window to preview the expression. There is no limit on the number of these windows which can be opened. -History of entered expressions is saved in an ArrayList within InputHandler. -History can be accessed by entering "$#" where # is the number of the expression you entered. -History can also be accessed from the file menu which will open up a new window where you can scroll through your past expressions and see what they look like. Note: There's a bug here where the window is the wrong size at first, still working on it. -Pressing "Quit" or closing the main window will now end the program and close all windows. This is so the program will terminate without leaving a bunch of preview or history windows open.

--- 04_picasso_cps108_fall09/view/Frame.java
+++ 04_picasso_cps108_fall09/view/Frame.java
@@ -2,9 +2,15 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.util.ResourceBundle;
+import javax.swing.AbstractAction;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import model.Expression;
+import commands.InputHandler;
 import commands.Reader;
 import commands.Evaluater;
 import commands.Writer;
@@ -13,11 +19,21 @@ import commands.Writer;
 @SuppressWarnings("serial")
 public class Frame extends JFrame
 {
+    InputHandler handler;
+    private static ResourceBundle myResources = ResourceBundle.getBundle("resources.English");
+    
     public Frame (String title, Dimension size)
     {
         setTitle(title);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        handler = new InputHandler();
+        
+        //FileMenu menu = new FileMenu(handler);
+        JMenuBar result = new JMenuBar();
+        result.add(makeFileMenu());
+        setJMenuBar(result);
+        
         // create GUI components
         Canvas canvas = new Canvas(this);
         canvas.setSize(size);
@@ -34,12 +50,44 @@ public class Frame extends JFrame
         commands.add(expressionChooser);
         commands.add(new Writer());
 
-        TextPanel textpanel = new TextPanel(canvas);
+        //TextPanel textpanel = new TextPanel(canvas);
+        TextPanel textpanel = new TextPanel(canvas, handler);
         
         // add our container to Frame and show it
         getContentPane().add(canvas, BorderLayout.CENTER);
         getContentPane().add(commands, BorderLayout.NORTH);
         getContentPane().add(textpanel, BorderLayout.SOUTH);
         pack();
+    }
+    
+    /**
+     * Create the file menu for the game.
+     */
+    protected JMenu makeFileMenu ()
+    {
+        JMenu result = new JMenu(myResources.getString("FileMenu"));
+
+        result.add(new AbstractAction(myResources.getString("OpenCommand"))
+        {
+            public void actionPerformed (ActionEvent ev)
+            {
+                //IMPLEMENT
+            }
+        });
+        result.add(new AbstractAction(myResources.getString("HistoryCommand"))
+        {
+            public void actionPerformed (ActionEvent ev)
+            {
+                handler.openHistoryWindow();
+            }
+        });
+        result.add(new AbstractAction(myResources.getString("QuitCommand"))
+        {
+            public void actionPerformed (ActionEvent ev)
+            {
+                System.exit(0);
+            }
+        });
+        return result;
     }
 }
