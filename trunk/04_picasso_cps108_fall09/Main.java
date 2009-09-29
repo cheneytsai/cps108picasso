f96d56becce9a0a5ff81a1d3db12855ad5fc0e3b
baboonoflife <baboonoflife@f0a4c0ce-a3c5-11de-bea2-1db044c32224>
2009-09-29 07:53:21 +0000

    Implemented Color, changed the preprocessor method in parser (formerly named addSpaces) to read from the resourcebundle Preprocessor.properties.

--- trunk/04_picasso_cps108_fall09/Main.java
+++ trunk/04_picasso_cps108_fall09/Main.java
@@ -13,9 +13,8 @@ public class Main
     public static final Dimension SIZE = Utils.FRAME_LARGE.size();
     public static final String TITLE = "PICASSO!";
 
-
     public static void main (String[] args)
-    {
+    {        
         TokenHandler.tokenMapGenerator();
         Frame frame = new Frame(TITLE, SIZE);
         frame.setVisible(true);
